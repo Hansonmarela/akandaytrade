a9138780d98606e5750bcf0aa20ebf98922f6c26
Hansonmarela <selasikofihamenu@gmail.com>
2025-03-04 02:24:33 +0000

    links-update

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -23,13 +23,13 @@ const Hero = () => {
               </p>
               <div className="flex flex-col sm:flex-row sm:space-x-4">
                 <Link
-                  href="https://nextjstemplates.com/templates/saas-starter-startup"
+                  href="/signup"
                   className="rounded-md bg-primary px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-primary/80"
                 >
                   Get Started Now
                 </Link>
                 <Link
-                  href="https://github.com/NextJSTemplates/startup-nextjs"
+                  href="/About"
                   className="mt-4 sm:mt-0 rounded-md bg-black px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-black/90 dark:bg-white/10 dark:text-white dark:hover:bg-white/5"
                 >
                   Explore Our Services
